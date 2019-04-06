539c507851f68567b13591ecbacf5286a1841d80
yono.ap <yono.ap@icloud.com>
2019-04-06 19:38:35 +0900

    Initial Commit

---
+++ Swift5Pod/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift5Pod
+//
+//  Created by yoshiyuki ono on 2019/04/06.
+//  Copyright © 2019 yono-ap. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Swift5PodTests/Swift5PodTests.swift
@@ -0,0 +1,34 @@
+//
+//  Swift5PodTests.swift
+//  Swift5PodTests
+//
+//  Created by yoshiyuki ono on 2019/04/06.
+//  Copyright © 2019 yono-ap. All rights reserved.
+//
+
+import XCTest
+@testable import Swift5Pod
+
+class Swift5PodTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
